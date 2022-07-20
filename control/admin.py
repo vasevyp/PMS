58aeb4a85909b95dbfe2c15cc64e8e5d03d56e04
Yury Vasev <vasevyp@mail.ru>
2022-07-20 15:10:58 +0300

    Созданы миграции моделей для приложений register и control + admin.py. База данных пустая.

--- control/admin.py
+++ control/admin.py
@@ -3,9 +3,46 @@ from  .models import BuyItem, StockItem, SaleProduct, OrderItem, DeliverItem
 
 # Register your models here.
 class BuyItemAdmin(admin.ModelAdmin):
-    list_display = ['code', 'name' ]
+    list_display = ['code', 'name', 'unit','unit_cost','quantity','cost' ]
     prepopulated_fields= {'slug': ('name',)}  
     save_on_top = True
-    search_fields = ['name',]
+    search_fields = ['code','name',]
     list_filter= ('name', 'code')
 admin.site.register(BuyItem, BuyItemAdmin)
+
+
+class StockItemAdmin(admin.ModelAdmin):
+    list_display = ['code', 'name', 'unit','unit_cost','open', 'sales', 'received', 'transfer', 'move', 'waste','actual','actual_cost' ]
+    prepopulated_fields= {'slug': ('name',)}  
+    save_on_top = True
+    search_fields = ['name', 'code']
+    list_filter= ('name', 'code')
+admin.site.register(StockItem, StockItemAdmin)
+
+
+class SaleProductAdmin(admin.ModelAdmin):
+    list_display = ['code', 'name', 'price','quantity','revenue' ]
+    prepopulated_fields= {'slug': ('name',)}  
+    save_on_top = True
+    search_fields = ['code','name',]
+    list_filter= ('name', 'code')
+admin.site.register(SaleProduct, SaleProductAdmin)
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['code', 'product', 'order_number', 'supplier','unit','order_quantity', 'created_date', 'status' ]
+    prepopulated_fields= {'slug': ('product','order_number')}  
+    save_on_top = True
+    search_fields = ['product', 'order_number']
+    list_filter= ('product', 'order_number')
+admin.site.register(OrderItem, OrderItemAdmin)
+
+
+
+class DeliverItemAdmin(admin.ModelAdmin):
+    list_display = ['code', 'product', 'order_number', 'supplier','unit','order_quantity', 'created_date', 'status' ]
+    prepopulated_fields= {'slug': ('product','order_number')}  
+    save_on_top = True
+    search_fields = ['product', 'order_number']
+    list_filter= ('product', 'order_number')
+admin.site.register(DeliverItem, DeliverItemAdmin)
--- register/admin.py
+++ register/admin.py
@@ -2,17 +2,15 @@ from django.contrib import admin
 
 from .models import Category, CategoryItem, Supplier, Item, Product, RecipeIngredient
 
-
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['code', 'name', ]
-    prepopulated_fields= {'slug': ('name',)}
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ['code', 'name' ]
+    prepopulated_fields= {'slug': ('name',)}  
     save_on_top = True
-    search_fields=['name',]
-    list_filter= ('name',)
-    
-    # class Meta:
-    #     ordering = ['code'] #Sort in desc order
-admin.site.register(Category, CategoryAdmin)
+    search_fields = ['name',]
+    list_filter= ('name', 'code')
+admin.site.register(Supplier, SupplierAdmin)
+
+
 
 class CategoryItemAdmin(admin.ModelAdmin):
     list_display = ['code', 'name', ]
@@ -26,14 +24,16 @@ class CategoryItemAdmin(admin.ModelAdmin):
     #     ordering = ['code'] #Sort in desc order
 admin.site.register(CategoryItem, CategoryItemAdmin)
 
-class SupplierAdmin(admin.ModelAdmin):
-    list_display = ['code', 'name' ]
-    prepopulated_fields= {'slug': ('name',)}  
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['code', 'name', ]
+    prepopulated_fields= {'slug': ('name',)}
     save_on_top = True
-    search_fields = ['name',]
-    list_filter= ('name', 'code')
-admin.site.register(Supplier, SupplierAdmin)
-
+    search_fields=['name',]
+    list_filter= ('name',)
+    
+    # class Meta:
+    #     ordering = ['code'] #Sort in desc order
+admin.site.register(Category, CategoryAdmin)
 class ItemAdmin(admin.ModelAdmin):
     list_display = ['code', 'name', 'category', 'supplier', 'unit_cost',] 
     prepopulated_fields= {'slug': ('name',)}  
@@ -41,15 +41,6 @@ class ItemAdmin(admin.ModelAdmin):
     search_fields = ('name',  'code')
     list_filter= ('name', 'code')
 admin.site.register(Item, ItemAdmin)
-
-
-class RecipeIngredientAdmin(admin.ModelAdmin):
-    list_display = ['recipe_name','ingredient', 'unit','quantity']
-    prepopulated_fields= {'slug': ('recipe_name',)}
-    save_on_top = True
-    search_fields = ['name__ingredient', ]
-    list_filter= ('recipe_name', 'ingredient')
-admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['code','name', ]
@@ -60,3 +51,11 @@ class ProductAdmin(admin.ModelAdmin):
  
     
 admin.site.register(Product, ProductAdmin)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ['recipe_name','ingredient', 'unit','quantity']
+    prepopulated_fields= {'slug': ('recipe_name',)}
+    save_on_top = True
+    search_fields = ['name__ingredient', ]
+    list_filter= ('recipe_name', 'ingredient')
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
+
